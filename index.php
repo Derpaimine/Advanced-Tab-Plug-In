105640c618957949133e53ba593f6780b575c36b
Jordan <jordandawson09@gmail.com>
2022-07-01 15:55:40 +0200

    Functionality done

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php 
+
+/*
+Plugin Name: Advanced tabs
+Description: Creates an advanced animated tab sorting plug in
+Version: 1.0.0
+Author: Jordan Dawson & Dale De Kock
+Text Domain:
+License: GPLv2 or later
+*/
+
+/*
+Copyright by Jordan Dawson & Dale De Kock
+*/
+
+include (plugin_dir_path( __FILE__) . 'script.js')
+include (plugin_dir_path( __FILE__) . 'style.css')
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,15 +24,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="script.js" defer></script>
-    <title>HTML/CSS/JS for Advanced Tabs Plugin</title>
     <link rel="stylesheet" href="style.css">
+    <title>HTML/CSS/JS for Advanced Tabs Plugin</title>
 </head>
 <body>
     <div class="navigation">
         <ul>
             <li class="list active" data-tab-target="#home">
                 <a href="#">
-                    <span class="icon">
+                    <span class="icon" >
                         <ion-icon name="home-outline"></ion-icon>
                     </span>
                     <span class="title">Home</span>
@@ -56,24 +75,24 @@
             </div>
         </ul>
     </div>
-    <div class="tab-content">
-        <div id="home">
+    <div class="tab-content active">
+        <div id="home" data-tab-content class="active">
             <h1>Home</h1>
             <p>Home Tings</p>
         </div>
-        <div id="profile">
+        <div id="profile" data-tab-content>
             <h1>Profile</h1>
             <p>Profile Tings</p>
         </div>
-        <div id="photos">
+        <div id="photos" data-tab-content>
             <h1>Photo</h1>
             <p>Photo Tings</p>
         </div>
-        <div id="messaging">
+        <div id="messaging" data-tab-content>
             <h1>Messaging</h1>
             <p>Messaging Tings</p>
         </div>
-        <div id="settings">
+        <div id="settings" data-tab-content>
             <h1>Settings</h1>
             <p>Settings Tings</p>
         </div>
--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const tabs = document.querySelectorAll('[data-tab-target')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+const contents= document.querySelector(".ta")
 
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
         target.classList.add('active')
-    })
+    });
 })
